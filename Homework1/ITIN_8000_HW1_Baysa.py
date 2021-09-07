a462a886a8cd193ae0e89fa56c00672920ca87b5
kkbaysa <57466485+kkbaysa@users.noreply.github.com>
2021-09-07 15:28:34 -0500

    Pseudo code for Homework 1

--- Homework1/ITIN_8000_HW1_Baysa.py
+++ Homework1/ITIN_8000_HW1_Baysa.py
@@ -1,1 +1,33 @@
-print(10)
+# This is my program for Homework 1 of ITIN 8000
+# The program is meant to take today's date and construct the parts of the date to print out
+# "Hello. Today's Date is [Month Name] [Day Number][th/nd/st/rd] of [Year].
+# The product of the month and day is [Month Number * Day], which is an [Odd/Even] number.
+# If you counted the days this month so far you would have
+# [Loop to count up to the current day of the month with each number on a new line] days.”
+# This work was done by Kaitlyn Baysa in Fall 2021.
+
+# Get today's date
+# Assign the month number to variable
+# Assign the month name to variable
+# Assign the day number to a variable
+# Assign the year number to a variable
+
+# Validate day to add correct suffix
+# If the day is equal to 1, 21, or 31 define suffix variable as "st"
+# If the day is equal to 2, or 22 define suffix variable as "nd"
+# If the day is equal to 3, 23 define suffix variable as "rd"
+# If it is any other day define suffix variable as "th"
+
+# Determine product of day and month
+# Assign product as "even" or "odd"
+# If the product of day number and month number modulus of 2 is zero (it is even) and define day_type as "even"
+# Else define product_type as "odd"
+
+# print the statement "Hello. Today's date is..." with variables
+# include a new line
+
+# create list of days with range
+# print the statement "If you counted..." with the list of days
+
+
+
