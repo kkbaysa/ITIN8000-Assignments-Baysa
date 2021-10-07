f493e9c774e4f547955750008a13172fb23bb0ac
kkbaysa <57466485+kkbaysa@users.noreply.github.com>
2021-10-07 00:44:24 -0500

    adding time to run

--- HW3ITIN8000Baysa/HW3part2.py
+++ HW3ITIN8000Baysa/HW3part2.py
@@ -63,5 +63,5 @@ if __name__ == '__main__':
     with open("ComicCharacters.json", "w") as fileOut:
         json.dump(jsonDump, fileOut)
 
-# print times of execution
-print(time.perf_counter())
+    # print times of execution
+    print(time.perf_counter())
